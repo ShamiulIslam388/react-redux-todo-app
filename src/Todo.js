b8a2aa2022a3b2db105a22c3f8f967b2f72eea4c
ShamiulIslam388 <58252309+ShamiulIslam388@users.noreply.github.com>
2021-02-28 22:44:04 +0600

    todo form added

--- src/Todo.js
+++ src/Todo.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from "react";
 
+function Todo() {
+  const [userInput, setUserInput] = useState("");
 
-function Todo(){
   return (
-    <div>Todo </div>
-  )
-  
+    <div>
+      <h2>Todo App Redux Demo</h2>
+      <form>
+        <input
+          type="text"
+          value={userInput}
+          onChange={(e) => setUserInput(e.target.value)}
+        />
+
+        <button>Add Todo</button>
+      </form>
+    </div>
+  );
 }
 
 export default Todo;
